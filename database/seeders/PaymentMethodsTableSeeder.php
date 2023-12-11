cd04810c0a73e9d36a34f4ba8433e22f9279641f
LAPTOP-976KCB5J\imamn <imam.nugraha@digitalcenter.id>
2023-12-11 21:54:46 +0700

    add account number in payment methods

--- database/seeders/PaymentMethodsTableSeeder.php
+++ database/seeders/PaymentMethodsTableSeeder.php
@@ -15,15 +15,51 @@ class PaymentMethodsTableSeeder extends Seeder
     public function run()
     {
         $paymentMethods = [
-            ['name' => 'Credit Card'],
-            ['name' => 'BCA'],
-            ['name' => 'BNI'],
-            ['name' => 'BSI'],
-            ['name' => 'MANDIRI'],
-            ['name' => 'PERMATA'],
-            ['name' => 'BRI']
+            [
+                'name' => 'UOB',
+                'account_number' => self::generateRandomAccountNumber()
+            ],
+            [
+                'name' => 'BCA',
+                'account_number' => self::generateRandomAccountNumber()
+            ],
+            [
+                'name' => 'BNI',
+                'account_number' => self::generateRandomAccountNumber()
+            ],
+            [
+                'name' => 'BSI',
+                'account_number' => self::generateRandomAccountNumber()
+            ],
+            [
+                'name' => 'MANDIRI',
+                'account_number' => self::generateRandomAccountNumber()
+            ],
+            [
+                'name' => 'PERMATA',
+                'account_number' => self::generateRandomAccountNumber()
+            ],
+            [
+                'name' => 'BRI',
+                'account_number' => self::generateRandomAccountNumber()
+            ]
         ];
 
         DB::table('payment_methods')->insert($paymentMethods);
+    }
+
+    private static  function generateRandomAccountNumber() {
+        $prefix = '123'; // Prefix example, change as needed
+        $suffix = ''; // You can generate a random number or string for the remaining part
+
+        // Generate random characters or numbers for the remaining part of the account number
+        $remainingLength = 10 - strlen($prefix); // Adjust this length according to your requirements
+
+        // Generate random characters or numbers for the suffix
+        for ($i = 0; $i < $remainingLength; $i++) {
+            $suffix .= mt_rand(0, 9); // Generates a random digit, you can adjust this according to your requirement
+        }
+
+        return $prefix . $suffix;
     }
 }
